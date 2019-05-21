af8d491968f06dd0f0cff9a91ff8df3946787834
victor110298 <44604893+victor110298@users.noreply.github.com>
2019-05-21 14:55:53 +0300

    small fix

--- src/main/java/com/mateacademy/springmvc/entity/CreatableEntity.java
+++ src/main/java/com/mateacademy/springmvc/entity/CreatableEntity.java
@@ -6,11 +6,12 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
 
 @MappedSuperclass
 @Setter
 @EntityListeners(value = UserListener.class)
 public class CreatableEntity {
     @Column(name = "created_date")
-    private Long createdDate;
+    private LocalDate createdDate;
 }
